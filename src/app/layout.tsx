588b7ddd6e49a73cca0e21871f1420ccd45d5544
Glauber Santos <Santos.Glauber2002@gmail.com>
2025-10-14 20:56:50 +0000

    Instale esse script na pagina <script type="text/javascript">     (funct

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -56,6 +56,17 @@ export default function RootLayout({
         `}}
         />
         {/* End Hotmart Widget */}
+
+        {/* Microsoft Clarity */}
+        <Script id="clarity-script" strategy="afterInteractive" dangerouslySetInnerHTML={{ __html: `
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "tq87ch8wxt");
+        `}}
+        />
+        {/* End Microsoft Clarity */}
       </head>
       <body className={cn("min-h-screen bg-background font-body antialiased")}>
         {/* Meta Pixel Code (noscript) */}
