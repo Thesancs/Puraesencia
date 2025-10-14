6ffaa0f6e6f7137e2b7f378f42c79c0de3d1e170
Glauber Santos <Santos.Glauber2002@gmail.com>
2025-10-14 18:58:01 +0000

    Esta muito grande ainda

--- src/components/landing-bano/CtaButton.tsx
+++ src/components/landing-bano/CtaButton.tsx
@@ -31,7 +31,7 @@ export default function CtaButton({
     "hotmart-fb hotmart__button-checkout",
     "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
     "w-full h-auto px-4 py-3 text-sm font-bold text-center text-primary-foreground transition-transform duration-300 ease-in-out transform rounded-lg shadow-2xl bg-primary hover:bg-primary/90 hover:scale-105 animate-pulse",
-    "sm:py-4 sm:text-sm",
+    "sm:py-4 sm:text-base",
     className,
     {
       "bg-destructive hover:bg-destructive/90": status === "error",
