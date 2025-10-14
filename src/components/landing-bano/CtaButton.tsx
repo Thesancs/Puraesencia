9eb72db25c608f76e7ee382a572797e56a9dc8ad
Glauber Santos <Santos.Glauber2002@gmail.com>
2025-10-14 18:56:31 +0000

    As letras ainda estão muito grandes e estao saindo dos botoes

--- src/components/landing-bano/CtaButton.tsx
+++ src/components/landing-bano/CtaButton.tsx
@@ -29,8 +29,9 @@ export default function CtaButton({
 
   const buttonClasses = cn(
     "hotmart-fb hotmart__button-checkout",
-    "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
-    "w-full h-auto py-3 text-sm sm:py-4 sm:text-base font-bold text-center text-primary-foreground transition-transform duration-300 ease-in-out transform rounded-lg shadow-2xl bg-primary hover:bg-primary/90 hover:scale-105 animate-pulse",
+    "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+    "w-full h-auto px-4 py-3 text-sm font-bold text-center text-primary-foreground transition-transform duration-300 ease-in-out transform rounded-lg shadow-2xl bg-primary hover:bg-primary/90 hover:scale-105 animate-pulse",
+    "sm:py-4 sm:text-sm",
     className,
     {
       "bg-destructive hover:bg-destructive/90": status === "error",
